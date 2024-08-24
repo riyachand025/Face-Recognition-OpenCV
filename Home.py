d4784e181acf97f150c96d7f06611d7a0cfa01da
riyachand025 <riyachand025@gmail.com>
2024-08-24 13:29:29 +0530

    home page

---
+++ Home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("Face Recognition Web App")
+st.divider()
+
+st.subheader(":blue[Pages:]")
+st.write("Home Page - For navigation")
+st.write("Detection- To detect the user")
+st.write("New Face - To register a new face")
+st.write("Users Page - To view all logged users")
+st.divider()
+
+st.subheader(":blue[Frameworks and Technologies used:]")
+st.write('OpenCV')
+st.write('json and csv files')
+st.write('numpy and pandas')
+st.write('streamlit')
+st.divider()
+
+st.write("Developed by :blue[RIYA CHAND]")
+st.write("Graphic Era Deemed To Be University")
+st.write("B.Tech CSE")
+st.write("University Roll Number - 2021237")
+st.divider()
